cc55d998066160d76e447e14b1c6b7b4af387641
Garrett Gomez <garrettgomez@Garretts-MBP-2.fios-router.home>
2021-02-16 17:31:04 -0500

    PyBank and PyPoll main.py are complete, working on making programs more efficient

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,45 +1,58 @@
 import os
 import csv
 
+# create path variable for read and write files
 py_bank = os.path.join('Resources', 'budget_data.csv')
+output_path = os.path.join('Analysis', 'financial_report.txt')
 
-
-month = []
-revenue = []
-
+# read in information from budget_data.csv
 with open(py_bank) as file_object:
     csvreader = csv.reader(file_object, delimiter=",")
-    #print(csvreader)
-
+    
+    # remove header from csv file
     header = next(csvreader)
     
+    # create empty lists to store values from csv file
+    month = []
+    revenue = []
+
+    # loop through data and store them in lists above
     for row in csvreader:
-        
         month.append(row[0])
         revenue.append(int(row[1]))
-        #print(f' {month} : {revenue}')
         
+# find total months in budget data       
 tot_month = len(month)
+
+# initalize variables for loops below
 net_total = 0
-ave_change = []
 tot_change = 0
+
+# create lists to store loop variable below
+ave_change = []
 new_month = []
 
+# loop through revenue list to find total revenue
 for value in revenue:
     net_total = net_total + value
 
+# loop to calculate difference from month to month
+# and to store difference and corresponding month in lists above
 for i in range(0,(tot_month-1)):
     diff = revenue[i+1] - revenue[i]
     ave_change.append(diff)
     new_month.append(month[i+1])
 
+# loop to calculate the precentage change
 for j in ave_change:
     tot_change = tot_change + j
     working = tot_change/len(ave_change)
 
+# Zip ave_change list and new_month list together
 zip_list = zip(new_month,ave_change)
 clan = list(zip_list)
 
+# Loop to find greatest increase and decrease in profit
 for x in clan:
     if x[1] == max(ave_change):
         g = x[0]       
@@ -48,10 +61,22 @@ for x in clan:
         m = x[0]
         n = x[1]
 
-
-print(f'\nTotal Months:  {tot_month}')
-print(f'Total:  {net_total}')
-print(f'Average Change: ${round(working,2)}')
-print(f'Greatest Increase: {g} : {h}')
-print(f'Greatest Decrease:  {m} : {n}')
+print("\nFinancial Analysis Report")
+print("___________________________")
+print(f'\nTotal Months:                 {tot_month}')
+print(f'Total:                        ${net_total}')
+print(f'Average Change:               ${round(working,2)}')
+print(f'Greatest Increase in Profit:  {g} : ${h}')
+print(f'Greatest Decrease in Profit:  {m} : ${n}')
 print('\n')
+
+with open(output_path, 'w') as output_file:
+    csvwriter = csv.writer(output_file)
+    
+    csvwriter.writerow(["Financial Analysis Report"])
+    csvwriter.writerow(["___________________________"])
+    csvwriter.writerow([f'Total Months:                 {tot_month}'])
+    csvwriter.writerow([f'Total:                        ${net_total}'])
+    csvwriter.writerow([f'Average Change:               ${round(working,2)}'])
+    csvwriter.writerow([f'Greatest Increase in Profit:  {g} : ${h}'])
+    csvwriter.writerow([f'Greatest Decrease in Profit:  {m} : ${n}'])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,27 +1,45 @@
 import os
 import csv
 
+# Create variable for file path and output path
 csvpath = os.path.join("Resources","election_data.csv")
+output_path = os.path.join("Analysis", "election_analysis.txt")
 
+# Convert CSV file into date to interpret with python
 with open(csvpath) as file_object:
     csvreader = csv.reader(file_object, delimiter=",")
     csvheader = next(csvreader)
     
+    # list to store individual voter information
     voter_id = []
     county = []
     voted_for = []
 
+    # for loop to append information into list above
     for row in csvreader:
         voter_id.append(row[0])
         county.append(row[1])
         voted_for.append(row[2])
 
+# total votes
 tot_votes = len(voter_id)
+
+# For loop to find number and name of candidates running for office
+candidates = []
+for y in voted_for:
+    if y in candidates:
+        continue
+    else:
+        candidates.append(y)
+
+# Zip 3 lists from csv file into 1 list
 zipper = zip(voter_id, county, voted_for)
 zip_list = list(zipper)
 
+# list to add number of votes per candidate (Khan, Correy, Li, O'Tooley)
 votes = [0,0,0,0]
-candidate = []
+
+# for loop to add votes per candidate
 for x in zip_list:
     if x[2] == "Khan":
         votes[0] += 1
@@ -32,17 +50,46 @@ for x in zip_list:
     else:
         votes[3] += 1
 
+# percentage of votes for each candidate
 khan = (votes[0]/tot_votes) * 100
 correy = (votes[1]/tot_votes) * 100
 li = (votes[2]/tot_votes) * 100
 tool = (votes[3]/tot_votes) * 100
 
-winner = max(votes)
+# Zip list together to find out winner of election
+zip_list2 = zip(candidates,votes)
+zipper2 = list(zip_list2)
 
+# Winner of election
+for z in zipper2:
+    if z[1] == max(votes):
+        winner = z[0]
+
+
+print("\nElection Results")
+print("_______________________________")
 print(f'\nTotal Votes:  {tot_votes}')
-print(f'\nKhan:          {round(khan,4)}% ({votes[0]})')
-print(f'Correy:        {round(correy,4)}% ({votes[1]})')
-print(f'Li:            {round(li,4)}% ({votes[2]})')
-print(f"O'Tooley:      {round(tool,4)}%  ({votes[3]})")
-print(f"Winner:    {winner}")
+print("_______________________________")
+print(f'\n{candidates[0]}:          {round(khan,4)}% ({votes[0]})')
+print(f'{candidates[1]}:        {round(correy,4)}% ({votes[1]})')
+print(f'{candidates[2]}:            {round(li,4)}% ({votes[2]})')
+print(f"{candidates[3]}:      {round(tool,4)}%  ({votes[3]})")
+print("_______________________________")
+print(f"\nWinner:        {winner}")
 print("\n")
+
+
+with open(output_path, 'w') as results:
+    csvwriter = csv.writer(results)
+    
+    csvwriter.writerow(["\nElection Results"])
+    csvwriter.writerow(["_______________________________"])
+    csvwriter.writerow([f'\nTotal Votes:  {tot_votes}'])
+    csvwriter.writerow(["_______________________________"])
+    csvwriter.writerow([f'\n{candidates[0]}:          {round(khan,4)}% ({votes[0]})'])
+    csvwriter.writerow([f'{candidates[1]}:        {round(correy,4)}% ({votes[1]})'])
+    csvwriter.writerow([f'{candidates[2]}:            {round(li,4)}% ({votes[2]})'])
+    csvwriter.writerow([f"{candidates[3]}:      {round(tool,4)}%  ({votes[3]})"])
+    csvwriter.writerow(["_______________________________"])
+    csvwriter.writerow([f"\nWinner:        {winner}"])
+    
